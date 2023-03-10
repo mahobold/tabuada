56109d1a2c13bc48d3720efe8715c022b61c6456
MARIA VITORIA MAGALHAES HOBOLD <hobold.7994@aluno.pr.senac.br>
2023-03-10 08:06:23 -0300

    tabuada

---
+++ tabuada.php
@@ -0,0 +1,82 @@
+<?php
+    $a=1;
+    $b=2;
+    $c=3;
+    $d=4;
+    $e=5;
+    $f=6;
+    $g=7;
+    $h=8;
+    $i=9;
+    $j=10;
+
+    do{
+        echo "Número: " . $a . "</br>";
+        $a = $a + 1;
+    }
+    while ($a <= 10);
+    echo ("</br>");
+
+    do{
+        echo "Número: " . $b . "</br>";
+        $b = $b + 2;
+    }
+    while ($b <= 20);
+    echo ("</br>");
+
+    do{
+        echo "Número: " . $c . "</br>";
+        $c = $c + 3;
+    }
+    while ($c <= 30);
+    echo ("</br>");
+
+    do{
+        echo "Número: " . $d . "</br>";
+        $d = $d + 4;
+    }
+    while ($d <= 40);
+    echo ("</br>");
+
+    do{
+        echo "Número: " . $e . "</br>";
+        $e = $e + 5;
+    }
+    while ($e <= 50);
+    echo ("</br>");
+
+    do{
+        echo "Número: " . $f . "</br>";
+        $f = $f + 6;
+    }
+    while ($f <= 60);
+    echo ("</br>");
+
+    do{
+        echo "Número: " . $g . "</br>";
+        $g = $g + 7;
+    }
+    while ($g <= 70);
+    echo ("</br>");
+
+    do{
+        echo "Número: " . $h . "</br>";
+        $h = $h + 8;
+    }
+    while ($h <= 80);
+    echo ("</br>");
+
+    do{
+        echo "Número: " . $i . "</br>";
+        $i = $i + 9;
+    }
+    while ($i <= 90);
+    echo ("</br>");
+
+    do{
+        echo "Número: " . $j . "</br>";
+        $j = $j + 10;
+    }
+    while ($j <= 100);
+    echo ("</br>");
+?>    
